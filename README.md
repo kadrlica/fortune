db83661bab70640a644df494c281a08ff0d893b9
Alex Drlica-Wagner <kadrlica@fnal.gov>
2017-02-09 05:31:01 -0300

    Updates for godb

--- README.md
+++ README.md
@@ -12,67 +12,68 @@ Add the executable to your path:
 
 Print a help message and exit:
 ```
-> fortune --help
-usage: fortune [-h] -p PSF -b {u,g,r,i,z,Y} -a AIRMASS [-f FUTURE] [-m MAX]
-               [-e] [-V]
+usage: fortune [-h] [-a AIRMASS] [-g] [-c [CUT]] [--ecs] [-V] fil secz psf
 
 Predict the future of your PSF...
 
+positional arguments:
+  fil                   current DECam filter
+  secz                  current airmass (secz)
+  psf                   current FWHM (arcsec)
+
 optional arguments:
   -h, --help            show this help message and exit
-  -p PSF, --psf PSF     current FWHM (arcsec)
-  -b {u,g,r,i,z,Y}, --band {u,g,r,i,z,Y}
-                        current DECam band
   -a AIRMASS, --airmass AIRMASS
-                        current airmass (secz)
-  -f FUTURE, --future FUTURE
-                        future airmass (secz)
-  -m MAX, --max MAX     maximum acceptable FWHM (arcsec)
-  -e, --ecs             for ECS only!!!
+                        desired airmass (secz)
+  -g, --grid            return a grid of airmasses
+  -c [CUT], --cut [CUT]
+                        maximum acceptable FWHM (arcsec)
+  --ecs                 for ECS eyes only!
   -V, --version         print version and exit.
+
 ```
 
-Create a grid of predicted PSFs based on the current psf (`--psf`), band (`--band`), and airmass (`--airmass`):
+Predict the PSF in the other filters:
 ```
-> fortune --psf 1.4 --band g --airmass 1.4
-Clear your mind and I will predict your future...
+> fortune g 1.4 1.4
+Clear your mind and I will predict the fortune of your PSF...
 
-Airmass:    1.0    1.2    1.4    1.6    1.8    2.0
---------------------------------------------------
-u-band :   1.20   1.34   1.47   1.59   1.71   1.82
-g-band :   1.14   1.28   1.40   1.52   1.63   1.73
-r-band :   1.08   1.20   1.32   1.43   1.54   1.64
-i-band :   1.04   1.16   1.27   1.38   1.48   1.57
-z-band :   1.00   1.12   1.23   1.33   1.43   1.52
-Y-band :   0.99   1.10   1.21   1.31   1.41   1.50
+Airmass:   1.40
+---------------
+u-band :   1.43
+g-band :   1.40
+r-band :   1.29
+i-band :   1.24
+z-band :   1.20
+Y-band :   1.17
 ```
 
 Specify a future airmass:
 ```
-> fortune --psf 1.4 --band g --airmass 1.4 --future 1.2
-Clear your mind and I will predict your future...
+> fortune g 1.4 1.4 -a 1.2
+Clear your mind and I will predict the fortune of your PSF...
 
-Airmass:    1.2
+Airmass:   1.20
 ---------------
-u-band :   1.34
+u-band :   1.31
 g-band :   1.28
-r-band :   1.20
-i-band :   1.16
-z-band :   1.12
-Y-band :   1.10
+r-band :   1.18
+i-band :   1.13
+z-band :   1.09
+Y-band :   1.07
 ```
 
-Specify a desired PSF:
+Output a grid of predicted PSFs color coded by a PSF cut:
 ```
-> ./fortune --psf 1.4 --band g --airmass 1.4 --max 1.4
-Clear your mind and I will predict your future...
+> fortune g 1.4 1.4 -g --cut 1.4 
+Clear your mind and I will predict the fortune of your PSF...
 
-Airmass:    1.0    1.2    1.4    1.6    1.8    2.0
---------------------------------------------------
-u-band :   1.20   1.34   1.47   1.59   1.71   1.82
-g-band :   1.14   1.28   1.40   1.52   1.63   1.73
-r-band :   1.08   1.20   1.32   1.43   1.54   1.64
-i-band :   1.04   1.16   1.27   1.38   1.48   1.57
-z-band :   1.00   1.12   1.23   1.33   1.43   1.52
-Y-band :   0.99   1.10   1.21   1.31   1.41   1.50
+Airmass:   1.00   1.20   1.20   1.40   1.60   1.80   2.00
+---------------------------------------------------------
+u-band :   1.17   1.31   1.31   1.43   1.55   1.66   1.77
+g-band :   1.14   1.28   1.28   1.40   1.52   1.63   1.73
+r-band :   1.06   1.18   1.18   1.29   1.40   1.50   1.60
+i-band :   1.01   1.13   1.13   1.24   1.34   1.44   1.54
+z-band :   0.98   1.09   1.09   1.20   1.30   1.39   1.49
+Y-band :   0.95   1.07   1.07   1.17   1.27   1.36   1.45
 ``
--- fortune.py
+++ fortune.py
@@ -2,62 +2,108 @@
 """
 Predict the future of your PSF...
 """
-__version__ = '1.0'
+__version__ = '1.1'
 
 import os
 from collections import OrderedDict as odict
 
-BANDS = odict([
-    ('u',380),
-    ('g',480),
-    ('r',640),
-    ('i',780),
-    ('z',920),
-    ('Y',990),
+BANDS = ['u','g','r','i','z','Y']
+GRID = [1.0, 1.2, 1.4, 1.6, 1.8, 2.0]
+
+# These were empirically derived to minimize the bias
+LAMBDA = odict([
+    ('u',380.),
+    ('g',425.),
+    ('r',630.),
+    ('i',780.),
+    ('z',920.),
+    ('Y',1050.),
+])
+
+# These come from the original filter curves
+LAMBDA_ORIG = odict([
+    ('u',380.),
+    ('g',480.),
+    ('r',640.),
+    ('i',780.),
+    ('z',920.),
+    ('Y',990.),
 ])
 
 COLORS = odict([
-   ('PURPLE'  ,'\033[95m'),
-   ('CYAN'    ,'\033[96m'),
-   ('DARKCYAN','\033[36m'),
-   ('BLUE'    ,'\033[94m'),
-   ('GREEN'   ,'\033[92m'),
-   ('YELLOW'  ,'\033[93m'),
-   ('RED'     ,'\033[91m'),
-   ('BOLD'    ,'\033[1m'),
+   ('PURPLE'   ,'\033[95m'),
+   ('CYAN'     ,'\033[96m'),
+   ('DARKCYAN' ,'\033[36m'),
+   ('BLUE'     ,'\033[94m'),
+   ('GREEN'    ,'\033[92m'),
+   ('YELLOW'   ,'\033[93m'),
+   ('RED'      ,'\033[91m'),
+   ('BOLD'     ,'\033[1m'),
    ('UNDERLINE','\033[4m'),
-   ('END'     ,'\033[0m'),
+   ('END'      ,'\033[0m'),
 ])
 
 MAX = None
+COLOR = False
 
 def color(s,color):
     color = color.upper()
     # This is not the best way to check for color support.
     # Look into 'tput colors'...
-    if 'xterm' in os.getenv('TERM'):
-        return COLORS[color] + s + COLORS['END']
-    else:
+    if 'xterm' not in os.getenv('TERM'):
         return s
+    elif not COLOR:
+        return s
+    else:
+        return COLORS[color] + s + COLORS['END']
 
-def fwhm(fwhm_now, lambda_now, secz_now, lambda_future, secz_future):
-    lambda_now, secz_now, lambda_future, secz_future = map(float,[lambda_now, secz_now, lambda_future, secz_future])
-    fwhm_future = fwhm_now * (lambda_future/lambda_now)**(-0.2) * (secz_future/secz_now)**(0.6)
-    return fwhm_future
+def fwhm(fwhm_now, lambda_now, secz_now, lambda_next, secz_next):
+    """Predict the fwhm.
 
-def fortune(fwhm_now, band_now, secz_now, secz_future=None):
-    lambda_now = BANDS[band_now]
+    Parameters:
+    -----------
+    fwhm_now    : The fwhm value of the current exposure
+    lambda_now  : The wavelength of the current exposure
+    secz_now    : The airmass of the current exposure
+    lambda_next : The wavelength of the next exposure
+    secz_next   : The airmass of the next exposure
+    
+    Returns:
+    --------
+    fwhm_next   : The predicted fwhm of the next exposure
+    """
+    lambda_now, secz_now = map(float,[lambda_now, secz_now])
+    lambda_next, secz_next = map(float,[lambda_next, secz_next])
+    fwhm_next = fwhm_now * (lambda_next/lambda_now)**(-0.2) * (secz_next/secz_now)**(0.6)
+    return fwhm_next
 
-    if secz_future is None:
-        secz_future = [1.0, 1.2, 1.4, 1.6, 1.8, 2.0]
-    elif isinstance(secz_future,(float,int,str)):
-        secz_future = [secz_future]
-    secz_future = sorted(secz_future)
+def fortune(fwhm_now, band_now, secz_now, secz_next=None):
+    """Print the fwhm prediction.
+    
+    Parameters:
+    -----------
+    fwhm_now :
+    band_now :
+    secz_now :
+    secz_next :
+    
+    Returns:
+    --------
+    None
+    """
+    lambda_now = LAMBDA[band_now]
 
-    intro = "Clear your mind and I will predict your future...\n"
+    if secz_next is None:
+        secz_next = [secz_now]
+    elif isinstance(secz_next,(float,int,str)):
+        secz_next = [secz_next]
+
+    secz_next = sorted(secz_next)
+
+    intro = "Clear your mind and I will predict the fortune of your PSF...\n"
     print(intro)
 
-    header = "%-7s:"%"Airmass"+''.join(['%7.1f'%s for s in secz_future])
+    header = "%-7s:"%"Airmass"+''.join(['%7.2f'%s for s in secz_next])
     print(header)
 
     hline = len(header)*"-"
@@ -68,19 +114,19 @@ def fortune(fwhm_now, band_now, secz_now, secz_future=None):
     future = []
     for b in BANDS:
         out = "%-7s:"%('%s-band'%b)
-        for i,secz in enumerate(secz_future):
-            lambda_future = BANDS[b]
-            fwhm_future = fwhm(fwhm_now,lambda_now, secz_now, lambda_future, secz)
-            fwhm_str = '%7.2f'%fwhm_future
-            future.append(fwhm_future)
+        for i,secz in enumerate(secz_next):
+            lambda_next = LAMBDA[b]
+            fwhm_next = fwhm(fwhm_now,lambda_now, secz_now, lambda_next, secz)
+            fwhm_str = '%7.2f'%fwhm_next
+            future.append(fwhm_next)
 
-            if fwhm_future < 3.0:
+            if fwhm_next < 3.0:
                 terrible = False
             if MAX is not None:
-                if fwhm_future > MAX:
+                if fwhm_next > MAX:
                     out += color(fwhm_str,'red')
                 else:
-                    out += color(fwhm_str,'green')
+                    out += color(fwhm_str,'darkcyan')
                     good = True
             else:
                 out += fwhm_str
@@ -100,24 +146,34 @@ def fortune(fwhm_now, band_now, secz_now, secz_future=None):
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('-p','--psf',required=True,default=None,type=float,
-                        help='current FWHM (arcsec)')
-    parser.add_argument('-b','--band',required=True,choices=BANDS.keys(),
-                        help='current DECam band')
-    parser.add_argument('-a','--airmass',required=True,default=None,type=float,
+    parser.add_argument('band',choices=BANDS,metavar='fil',
+                        help='current DECam filter')
+    parser.add_argument('secz',default=None,type=float,
                         help='current airmass (secz)')
-    parser.add_argument('-f','--future',default=None,action='append',type=float,
-                        help='future airmass (secz)')
-    parser.add_argument('-m','--max',default=None,type=float,
+    parser.add_argument('psf',default=None,type=float,
+                        help='current FWHM (arcsec)')
+    parser.add_argument('-a','--airmass',default=None,action='append',
+                        type=float,help='desired airmass (secz)')
+    parser.add_argument('-g','--grid',default=None,action='store_true',
+                        help='return a grid of airmasses')
+    parser.add_argument('-c','--cut',nargs='?',const=1.1,
+                        default=None,type=float,
                         help='maximum acceptable FWHM (arcsec)')
-    parser.add_argument('-e','--ecs',action='store_true',
-                        help='for ECS only!!!')
-    parser.add_argument('-V','--version',action='version',version='%(prog)s '+__version__,
+    parser.add_argument('--ecs',action='store_true',
+                        help='for ECS eyes only!')
+    parser.add_argument('-V','--version',action='version',
+                        version='%(prog)s '+__version__,
                         help='print version and exit.')
 
     args = parser.parse_args()
-    MAX = args.max
-    fortune(args.psf,args.band,args.airmass,args.future)
+    MAX   = args.cut
+    COLOR = args.cut
+
+    if args.airmass: airmass = args.airmass
+    else:            airmass = [args.secz]
+    if args.grid:    airmass += GRID
+        
+    fortune(args.psf,args.band,args.secz,airmass)
 
     if args.ecs:
         print('\n'+color("You're the best!",'bold'))
